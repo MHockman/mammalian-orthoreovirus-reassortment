776e21a2aa5e7f0c8cf7ddaea5ad1efafb9a5020
MHockman <MRachelle09@gmail.com>
2021-12-14 10:06:49 -0500

    All code used in the generation of figures for "Mammalian orthoreovirus reassortment proceeds with little constraint on segment mixing" including data files, model code, and pairwise association analysis code.

---
+++ Fig1_Fig2_Fig4_Fig5_Fig6_Code_InclReassortmentModel.R
@@ -0,0 +1,301 @@
+library(reshape2)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(gridExtra)
+library(readr)
+library(gtable)
+library(ggnewscale)
+library(scales)
+
+#Color Presets
+Predictioncolor <- "#841c1c"
+
+T3Dcolor <- "#220e1c"
+T3DcolorLite <- "#872e6c"
+T3DS208Pcolor <- "#548894"
+
+Nococolor <- "#c8e07a"
+
+T1Lcolor <- "#009cb9"
+T1LcolorLite <- "#00ccf2"
+T1LP208Scolor <- "#e78e10"
+T1Lvarcolor <- "#df3522"
+
+#Set basic plot parameters
+BasePlot_legend <- ggplot(data = NULL) +
+  theme(aspect.ratio = 1, 
+        plot.title = element_text(size = 40),
+        axis.text.x = element_text(size = 30),
+        axis.text.y = element_text(size = 30),
+        axis.title = element_text(size = 40),
+        axis.title.x = element_text(vjust = -1),
+        axis.title.y = element_text(vjust = 5),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 30),
+        legend.position = c(0.2, 0.9),
+        legend.background=element_blank(),
+        legend.key = element_blank(),
+        plot.margin = margin(10, 10, 50, 50)) 
+
+#Get the Data
+AllSerotypes_ReassortmentData_NarrowGate <- read_csv("data/AllSerotypes_ReassortmentData_NarrowGate.csv")
+
+T1L_Growth_Curve_CSV <- read_csv("data/T1L Growth Curve CSV.csv")
+T1L_Growth <- melt(T1L_Growth_Curve_CSV, id.vars = c("Time", "Virus"))
+
+#Plot Figure 1###########################################################################################
+Fig_1A_T1LGrowthCurve <- BasePlot_legend +
+  geom_smooth(data = T1L_Growth, aes(x = Time, y = log10(value), color = Virus)) +
+  geom_point(data = T1L_Growth, aes(x = Time, y = log10(value), color = Virus)) +
+  scale_color_manual(values = c(T1Lvarcolor, T1Lcolor)) +
+  labs(x = "Time (h)",
+       y = expression('Titer (log'[10]*'(PFU/mL))'))
+Fig_1A_T1LGrowthCurve
+
+ggsave("1A_T1LGrowthCurve_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)
+
+#Plot Figure 2##########################################################################################
+
+#Generate model predictions of reassortment
+moilist <- seq(-1, 2.5, by = 0.05)
+moilist <- 10^moilist 
+
+reassortment.function <- function(mu){
+  
+  limit <- qpois(0.9999999999, lambda = mu)
+  reassortment <- 0
+  numsum <- 0
+  denomsum <- 0
+  totvir <- 0
+  
+  for(i in 0:limit)
+  {
+    pi <- dpois(i, lambda = 0.5*mu)
+    
+    for (j in 0:limit){
+      
+      pj <- dpois(j, lambda = 0.5*mu)
+      
+      totvir <- i + j
+      
+      if(totvir != 0){
+        
+        c <- k * totvir
+        
+        denom <- pi*pj*c
+        numer <- denom*(1-((i/(i+j))^10) - ((j/(i+j))^10))
+        
+        numsum <- numsum + numer
+        denomsum <- denomsum + denom
+        
+      }
+      
+    }
+    
+  }
+  
+  reassortment <- numsum/denomsum
+  return(reassortment)
+}
+
+k <- 11
+T3Dreassortment_prediction <- sapply(X = moilist, FUN = reassortment.function)
+T3Dreassortment_prediction.df <- data.frame(reassortment = T3Dreassortment_prediction,
+                                            MOI = moilist)
+
+k <- 93
+T1Lreassortment_prediction <- sapply(X = moilist, FUN = reassortment.function)
+T1Lreassortment.df_prediction <- data.frame(reassortment = T1Lreassortment_prediction,
+                                            MOI = moilist)
+
+Figure_2A_T1LReassortment <- BasePlot_legend +
+  geom_line(data = T1Lreassortment.df_prediction, size = 3, color = Predictioncolor, aes(x = MOI, y = reassortment, lty = 'Prediction')) +
+  geom_point(data = subset(AllSerotypes_ReassortmentData_NarrowGate, Virus %in% "T1L"), color = T1Lcolor, size = 6, aes(x = MOI, y = PercR)) +
+  scale_x_log10(limits = c(NA, 320)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  theme(legend.title = element_blank())+
+  labs(title = "T1L",
+       x = "\nMOI (PFU/cell)",
+       y = "Proportion reassortant\n")
+Figure_2A_T1LReassortment  
+
+ggsave("2A_T1LReassortment_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)
+
+
+Figure_2B_T3DReassortment <- BasePlot_legend +
+  geom_line(data = T3Dreassortment_prediction.df, size = 3, color = Predictioncolor, aes(x = MOI, y = reassortment, lty = 'Prediction')) +
+  geom_point(data = subset(AllSerotypes_ReassortmentData_NarrowGate, Virus %in% "T3D"), color = T3Dcolor, size = 6, aes(x = MOI, y = PercR)) +
+  theme(legend.title = element_blank()) +
+  scale_x_log10(limits = c(NA, 320)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(title = "T3D", 
+       x = "\nMOI (PFU/cell)",
+       y = "Proportion reassortant\n")
+Figure_2B_T3DReassortment
+
+ggsave("2B_T3DReassortment_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)  
+
+#Plot Figure 4#####################################################################################
+
+Figure_4C_T1LReassortment <- BasePlot_legend +
+  geom_point(data = subset(AllSerotypes_ReassortmentData_NarrowGate, Virus %in% c("T1L", "T1LP208S")), size = 6, aes(x = MOI, y = PercR, color = Virus)) +
+  scale_color_manual(values = c(T1Lcolor, T1LP208Scolor)) +
+  scale_x_log10(limits = c(NA, 320)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(title = "T1L", 
+       x = "\nMOI (PFU/cell)",
+       y = "Proportion reassortant\n")
+Figure_4C_T1LReassortment
+
+ggsave("4C_T1LReassortmentMorphology_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)  
+
+Figure_4D_T3DReassortment <- BasePlot_legend +
+  geom_point(data = subset(AllSerotypes_ReassortmentData_NarrowGate, Virus %in% c("T3D", "T3DS208P")), size = 6, aes(x = MOI, y = PercR, color = Virus))+
+  scale_color_manual(values = c(T3Dcolor, T3DS208Pcolor)) +
+  scale_x_log10(limits = c(NA, 320)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(title = "T3D", 
+       x = "\nMOI (PFU/cell)",
+       y = "Proportion reassortant\n")
+Figure_4D_T3DReassortment
+
+ggsave("4D_T3DReassortmentMorphology_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)  
+
+#Data for 4E
+AllSerotypes_PercI_Replicates_Narrow <- read_csv("data/AllSerotypes_PercI_Replicates_Narrow.csv")
+T3DPerci <- subset(AllSerotypes_PercI_Replicates_Narrow, Virus == "T3D")
+T3DS208PPerci <- subset(AllSerotypes_PercI_Replicates_Narrow, Virus == "T3DS208P")
+T1LPerci <- subset(AllSerotypes_PercI_Replicates_Narrow, Virus == "T1L")
+T1LP208SPerci <- subset(AllSerotypes_PercI_Replicates_Narrow, Virus == "T1LP208S")
+
+
+Ratios.T3D.df <- data.frame(Serotype = "T3D",
+                            MOI = T3DPerci$MOI,
+                            Ratio = T3DS208PPerci$PercI/T3DPerci$PercI)
+Ratios.T1L.df <- data.frame(Serotype = "T1L",
+                            MOI = T1LPerci$MOI,
+                            Ratio = T1LP208SPerci$PercI/T1LPerci$PercI)
+
+Ratios <- bind_rows(Ratios.T3D.df, Ratios.T1L.df)
+Ratios_average <- aggregate(Ratio ~ Serotype, Ratios, mean)
+
+Figure_4E_InfectionRatios <- BasePlot_legend +
+  geom_col(data = Ratios_average, aes(Serotype, Ratio, fill = Serotype)) +
+  geom_point(data = Ratios, size = 3, aes(x = Serotype, y = Ratio, color = Serotype)) +
+  scale_color_manual(values = c(T3DcolorLite, T1LcolorLite)) +
+  scale_fill_manual(values = c(T3Dcolor, T1Lcolor)) +
+  geom_point(color = "black", pch = 21, size = 3.3, data = Ratios, aes(x = Serotype, y = Ratio)) +
+  theme(legend.position = "None") +
+  scale_y_continuous(limits = c(0, 1.5)) +
+  labs(x = "\nSerotype",
+       y = "Fold change in \nproportion infected cells\n")
+Figure_4E_InfectionRatios
+
+ggsave("4E_InfectionRatios_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)  
+
+Figure_4F_AllSerotypeReassortment <- BasePlot_legend +
+  geom_point(data = subset(AllSerotypes_ReassortmentData_NarrowGate, Virus %in% c("T1L", "T1LP208S", "T3D", "T3DS208P")), size = 6, aes(x = PercI, y = PercR, color = Virus)) +
+  scale_color_manual(values = c(T1Lcolor, T1LP208Scolor, T3Dcolor, T3DS208Pcolor)) +
+  theme(legend.position = c(0.8, 0.2)) +
+  scale_x_continuous(limits = c(0, 1)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(x = "\nProportion infected cells",
+       y = "Proportion reassortant\n")
+Figure_4F_AllSerotypeReassortment
+
+ggsave("4F_ReassortmentvsPercI_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)
+
+#Plot Figure 5##################################################################################
+
+#Get Data for Figure 5
+AllSerotypes_PercI_Replicates_Narrow <- read_csv("data/AllSerotypes_PercI_Replicates_Narrow.csv")
+
+AllSerotypes_Averages <- aggregate(AllSerotypes_PercI_Replicates_Narrow$PercI, 
+                                   by = list(AllSerotypes_PercI_Replicates_Narrow$MOI, AllSerotypes_PercI_Replicates_Narrow$Virus), 
+                                   mean, na.rm = TRUE)
+
+T3DPerci <- subset(AllSerotypes_PercI_Replicates_Narrow, Virus == "T3D")
+T3DS208PPerci <- subset(AllSerotypes_PercI_Replicates_Narrow, Virus == "T3DS208P")
+T1LPerci <- subset(AllSerotypes_PercI_Replicates_Narrow, Virus == "T1L")
+T1LP208SPerci <- subset(AllSerotypes_PercI_Replicates_Narrow, Virus == "T1LP208S")
+
+#Get predicted infection levels based on Poisson
+moilist <- seq(-1, 2.5, by = 0.05)
+moilist <- 10^moilist 
+percipredicted <- 1 - exp(-moilist)
+percipredicted_adjusted <- 1 - exp(-0.4 * moilist)
+prediction.df <- data.frame(moi = moilist,
+                            perci = percipredicted,
+                            perciadj = percipredicted_adjusted)
+
+prediction.df <- melt(prediction.df, id.vars = "moi")
+
+#Plot the Data
+Figure_5A_PercentInfectionData <- BasePlot_legend +
+  geom_line(data = AllSerotypes_Averages, size = 1.5, aes(x = Group.1, y = x, color = Group.2)) +
+  scale_color_manual(values = c(T1Lcolor, T1LP208Scolor, T3Dcolor, T3DS208Pcolor)) +
+  new_scale_color() +
+  geom_point(data = AllSerotypes_PercI_Replicates_Narrow, size = 3, aes(x = MOI, y = PercI, color = Virus)) +
+  scale_color_manual(values = c(T1Lcolor, T1LP208Scolor, T3Dcolor, T3DS208Pcolor), guide = 'none') +
+  geom_point(color = "black", pch = 21, size = 3.3, data = AllSerotypes_PercI_Replicates_Narrow, aes(x = MOI, y = PercI)) +
+  new_scale_color() +
+  geom_line(size = 1.5, data = prediction.df, aes(x = moi, y = value, color = variable)) +
+  scale_color_manual(labels = c("Prediction\n", "Adjusted\nPrediction"), values = c("#AB2208", "#F76F56")) +
+  theme(legend.text = element_text(size = 25),
+        legend.position = c(0.15, 0.75),)+
+  scale_x_log10(limits = c(NA, 320)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(title = "", 
+       x = "\nMOI (PFU/cell)",
+       y = "Proportion infected cells\n",
+       color = "Virus")  
+Figure_5A_PercentInfectionData  
+
+ggsave("5A_PercIvsPrediction_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)
+
+#Get the Data for 5B
+AllSerotypes_Invsout_All_Replicates_NarrowGate <- read_csv("data/AllSerotypes_Invsout_All Replicates_NarrowGate.csv")
+
+Figure_5B_InputvsOutput <- BasePlot_legend +
+  geom_point(data = AllSerotypes_Invsout_All_Replicates_NarrowGate, aes(x = MOI, y = Output, color = Virus), size = 3) +
+  scale_color_manual(values = c(T1Lcolor, T1LP208Scolor, T3Dcolor, T3DS208Pcolor)) +
+  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(.x))) +
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(.x))) +
+  labs(title = "", 
+       x = expression("MOI (log"[10]*"PFU/cell)"),
+       y = expression("Virus output (log"[10]*"PFU)"),
+       #y = expression("log"[10]*"(Virus output (PFU))"),
+       color = "Virus")
+Figure_5B_InputvsOutput
+
+ggsave("5B_InputvsOutput_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)
+
+#Plot Figure 6######################################################################################
+
+Figure_6C_T3DvsT3DNocoReassortment <- BasePlot_legend +
+  geom_point(data = subset(AllSerotypes_ReassortmentData_NarrowGate, Virus %in% c("T3D", "T3D Noco")), size = 6, aes(x = MOI, y = PercR, color = Virus)) +
+  scale_color_manual(values = c(T3Dcolor, Nococolor)) +
+  scale_x_log10(limits = c(NA, 320)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(title = "T3D", 
+       x = "\nMOI (PFU/cell)",
+       y = "Proportion reassortant\n")
+Figure_6C_T3DvsT3DNocoReassortment     
+
+ggsave("6C_T3DvsNoco_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)
+
+Figure_6D_T3DS208PvsT3DS208PNocoReassortment <- BasePlot_legend +
+  geom_point(data = subset(AllSerotypes_ReassortmentData_NarrowGate, Virus %in% c("T3DS208P", "T3DS208P Noco")), size = 6, aes(x = MOI, y = PercR, color = Virus)) +
+  scale_color_manual(values = c(T3DS208Pcolor, Nococolor)) +
+  scale_x_log10(limits = c(NA, 320)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  theme(legend.position = c(0.3, 0.9)) +
+  labs(title = "T3DS208P", 
+       x = "\nMOI (PFU/cell)",
+       y = "Proportion reassortant\n")
+Figure_6D_T3DS208PvsT3DS208PNocoReassortment  
+
+ggsave("6D_T3DS208PvsT3DS208PNocoReassortmen_ReviewerRevision.tiff", width = 10, height = 10, dpi = 1200)
---
+++ Fig3_PairwiseAssociationAnalysis.R
@@ -0,0 +1,337 @@
+# Meg Reo Pairwise Association Analysis
+
+require(MASS)
+require(tidyverse)
+require(lmerTest)
+require(robustlmm)
+require(latex2exp)
+require(ggthemes)
+require(ggridges)
+require(reshape2)
+
+
+theme_set(theme_grey())
+
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  legend
+}
+Proj.Home = "" #Set Home Location here
+Data.Dir = file.path(Proj.Home,"data")
+setwd(Data.Dir)
+
+MRH3 = read.csv(file = file.path(Proj.Home,"data", "T3D_Reassortment_PairwiseFormat.csv"), header = TRUE, fileEncoding = "UTF-8-BOM") %>%
+  na.omit() %>%
+  mutate(L1 = L1 %>% as.numeric,
+         L2 = L2 %>% as.numeric,
+         L3 = L3 %>% as.numeric,
+         M1 = M1 %>% as.numeric,
+         M2 = M2 %>% as.numeric,
+         M3 = M3 %>% as.numeric,
+         S1 = S1 %>% as.numeric,
+         S2 = S2 %>% as.numeric,
+         S3 = S3 %>% as.numeric,
+         S4 = S4 %>% as.numeric)
+
+L1.Index = which(colnames(MRH3) == "L1")
+
+Reps = table(MRH3$Rep) %>% names 
+MOIs = table(MRH3$MOI) %>% names
+
+#Generate empty data frame with desired columns
+Corr = MRH3 %>% filter(Rep == 1, 
+                       MOI == 100) %>%
+  select(L1:S4) %>% cor %>% melt %>%
+  mutate()
+
+Corr = Corr[0,] #Isolate headers
+
+for (rep in 1:length(Reps)) {     # Iterate through Replicates
+  for (moi in 1:length(MOIs)) {   # Iterate through MOIs
+    Corr = rbind(Corr, #Append data from current MOI and Replicate to "Corr" data frame
+                 MRH3 %>% filter(Rep == Reps[rep],
+                                 MOI == MOIs[moi]) %>%
+                   select(L1:S4) %>% #Select the columns containing presence/absence flags for each segment
+                   cor %>% #Calculate correlations between each segment pair
+                   melt %>% # Convert correlation matrix to long form (Segment 1, Segment 2, Correlation, and MOI)
+                   mutate(MOI = MOIs[moi],
+                          Rep = Reps[rep]))
+  }
+}
+
+Reo.Corr = Corr %>% 
+  na.omit() %>%
+  rename(X = Var1,
+         Y = Var2,
+         r = value) %>%
+  mutate(MOI = MOI %>% as.numeric,
+         r2 = r * r)
+
+Corr = Corr[0,] #Empty data frame, retain only headers
+
+for (moi in 1:length(MOIs)) { # Iterate through MOIs, but data from all replicates are pooled
+  Corr = rbind(Corr,
+               MRH3 %>% filter(MOI == MOIs[moi]) %>%
+                 select(L1:S4) %>% 
+                 cor %>%
+                 melt %>%
+                 mutate(MOI = MOIs[moi]))
+}
+
+Pooled.df = Corr %>% rename(X = Var1,
+                            Y = Var2,
+                            r = value) %>%
+  mutate(MOI = MOI %>% as.numeric,
+         r2 = r * r)
+
+Pooled.df2 = Pooled.df %>% filter(X != Y) %>% na.omit()
+
+# Export data ----
+write.csv(Reo.Corr, file = file.path(Proj.Home,"data", "T3D_Segment_Associations.csv"), row.names = FALSE)
+write.csv(Pooled.df2, file = file.path(Proj.Home,"data","T3D_Pooled_Associations.csv"), row.names = FALSE)
+
+# Base plots ----
+Base.Plot = ggplot() +
+  theme(text=element_text(size=14,face="bold"),
+        strip.text.x=element_text(size=rel(1.5),margin=margin(0,0,3,0)),
+        strip.text.y=element_text(size=rel(1.5),margin=margin(0,0,0,0),angle=0),
+        strip.background = element_blank(),
+        plot.title = element_text(size=rel(1.5)),
+        axis.text.x=element_text(angle=0,vjust=0,size=rel(1.5),color = "black"),
+        axis.text.y=element_text(size=rel(1.5),color = "black"),
+        axis.line.x = element_line(size=0.5),
+        axis.line.y = element_line(size=0.5),
+        axis.ticks.x = element_line(size=0.5),
+        axis.ticks.y = element_line(size = 0.5),
+        axis.title.y = element_text(size=rel(1.2),color = "black"),
+        axis.title.x = element_text(size=rel(1.2)),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank())
+
+Stair.Plot = ggplot() +
+  theme(text=element_text(size=10,face="bold"),
+        plot.title=element_text(size=rel(2),face="bold"),
+        legend.title.align=3,
+        legend.title=element_text(size=rel(1.5),face="bold",vjust=0),
+        legend.text=element_text(size=rel(1.2),face="bold",vjust=0),
+        legend.key.width=unit(2,"cm"),
+        axis.text.x = element_text(size=rel(1),vjust = .5,hjust=.5,face="bold",color = "black", margin=margin(5,0,0,0), angle=90),
+        axis.text.y = element_text(size=rel(1),face="bold",color = "black"),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.line=element_blank(),
+        strip.text.x = element_text(size=rel(1),angle=0,face="bold"),
+        strip.text.y = element_text(size=rel(1),angle=0,face="bold"),
+        strip.background = element_blank(),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background = element_blank())
+
+# Stair Plot ----
+
+Stair.Plot +
+  geom_tile(data = Pooled.df,
+            aes(x = X,
+                y = Y,
+                fill = r2)) +
+  facet_wrap(~MOI) +
+  coord_equal() +
+  scale_fill_viridis_c(option = "inferno") +
+  labs(x = NULL,
+       y = NULL,
+       fill = TeX("\\textbf{$\\r^2$}")) +
+  theme(legend.position = "NA")
+
+ggsave(file = "T3D_Association_Stair_Plot.tiff",
+       width = 5,
+       height = 4,
+       dpi = 1200)
+
+Stair.Plot +
+  geom_tile(data = Reo.Corr,
+            aes(x = X,
+                y = Y,
+                fill = r2)) +
+  facet_grid(Rep~MOI) +
+  coord_equal() +
+  scale_fill_viridis_c(option = "inferno") +
+  labs(x = NULL,
+       y = NULL,
+       fill = TeX("\\textbf{$\\r^2$}")) +
+  theme(legend.position = "NA")
+
+ggsave(file = "T3D_Association_Stair_Plot_Individual.tiff",
+       width = 8,
+       height = 4,
+       dpi = 1200)
+
+Legend.Plot = Stair.Plot +
+  geom_tile(data = Pooled.df,
+            aes(x = X,
+                y = Y,
+                fill = r2)) +
+  facet_wrap(~MOI) +
+  coord_equal() +
+  scale_fill_viridis_c(option = "inferno") +
+  labs(x = NULL,
+       y = NULL,
+       fill = TeX("\\textbf{$\\r^2$}")) +
+  theme(legend.position = "bottom")
+
+ggsave(g_legend(Legend.Plot),
+       file = "T3D_Association_Stair_Plot_Legend.tiff",
+       width = 5,
+       height = 1,
+       dpi = 1200)
+
+# Ridge Plot ----
+Base.Plot +
+  geom_density_ridges(data = Pooled.df2,
+                      aes(y = factor(MOI),
+                          x = r2),
+                      alpha = 0.8,
+                      fill = "navy") +
+  labs(x = TeX("\\textbf{Pairwise association ($\\r^2 $)}"),
+       y = "Multiplicity of infection (PFU/cell)") +
+  coord_cartesian(xlim = c(0,1))
+
+ggsave(file = "T3D_Association_Ridge_Plot.tiff",
+       width = 6,
+       height = 4.5,
+       dpi = 1200)
+
+Base.Plot +
+  geom_density_ridges(data = Reo.Corr,
+                      aes(y = factor(MOI),
+                          x = r2),
+                      alpha = 0.8,
+                      fill = "navy") +
+  facet_wrap(~Rep) +
+  labs(x = TeX("\\textbf{Pairwise association ($\\r^2 $)}"),
+       y = "Multiplicity of infection (PFU/cell)") +
+  coord_cartesian(xlim = c(0,1)) +
+  theme(panel.spacing = unit(0.2, "in"))
+
+ggsave(file = "T3D_Association_Ridge_Plot_Facet.tiff",
+       width = 10,
+       height = 4.5,
+       dpi = 1200)
+
+Base.Plot +
+  geom_density_ridges(data = Reo.Corr,
+                      aes(y = factor(MOI),
+                          x = r2,
+                          fill = Rep),
+                      alpha = 0.6) +
+  scale_fill_tableau() +
+  labs(x = TeX("\\textbf{Pairwise association ($\\r^2 $)}"),
+       y = "Multiplicity of infection (PFU/cell)") +
+  coord_cartesian(xlim = c(0,1)) +
+  theme(panel.spacing = unit(0.2, "in"))
+
+ggsave(file = "T3D_Association_Ridge_Plot_Individual.tiff",
+       width = 6,
+       height = 4.5,
+       dpi = 1200)
+
+# Scatter Plot ----
+
+Base.Plot +
+  geom_jitter(data = Pooled.df %>% filter(X != Y),
+              aes(x = MOI %>% log10,
+                  y = r2)) +
+  scale_color_tableau() +
+  labs(x = TeX("\\textbf{MOI ($\\log_1_0$ PFU/cell)}"),
+       y = TeX("\\textbf{Pairwise association ($\\r^2 $)}"))
+
+ggsave(file = "T3D_Association_Scatter_Plot_Pooled.tiff",
+       width = 6,
+       height = 4.5,
+       dpi = 1200)
+
+Base.Plot +
+  geom_jitter(data = Reo.Corr %>% filter(X != Y) %>% mutate(Pair = str_c(X,Y)),
+              aes(x = MOI %>% log10,
+                  y = r2)) +
+  geom_smooth(data = Reo.Corr %>% 
+                filter(X != Y) %>% 
+                mutate(Pair = str_c(X,Y)) %>%
+                filter(Pair %in% c("L1L2","L1M2")),
+              aes(x = MOI %>% log10,
+                  y = r2,
+                  color = Pair),
+              fill = NA) +
+  geom_point(data = Reo.Corr %>% 
+               filter(X != Y) %>% 
+               mutate(Pair = str_c(X,Y)) %>%
+               filter(Pair %in% c("L1L2","L1M2")),
+             aes(x = MOI %>% log10,
+                 y = r2,
+                 color = Pair),
+             size = 2) +
+  scale_color_tableau() +
+  labs(x = TeX("\\textbf{MOI ($\\log_1_0$ PFU/cell)}"),
+       y = TeX("\\textbf{Pairwise association ($\\r^2 $)}"))
+
+ggsave(file = "T3D_Association_Scatter_Plot_Individual.tiff",
+       width = 6,
+       height = 4.5,
+       dpi = 1200)
+
+Sig.Test = Pooled.df %>%
+  filter(X != Y) %>%
+  mutate(Pair = str_c(X,Y)) %>%
+  group_by(X, Y) %>%
+  summarize(Cumulative = sum(r2)) %>% #Sum of r2. Higher sum -> association decays more slowly as MOI increases. Could also use area under curve.
+  ungroup %>%
+  mutate(Z = (Cumulative - mean(Cumulative)) / sd(Cumulative),
+         p = pmin(2*pnorm(abs(Z), mean = 0, sd = 1, lower.tail = FALSE),rep(1,90)), #2*p for two-tailed test
+         index = 1:90) 
+# Note, these p-values are uncorrected. Of 45 comparisons, 3 with p < 0.05 is not necessarily unexpected
+
+Sig.Test %>% filter (p< 0.05)
+Sig.Test
+
+Stair.Plot +
+  geom_tile(data = Sig.Test %>% mutate(X = as.character(X),
+                                       Y = as.character(Y)) %>%
+              filter(X > Y),
+            aes(x = X,
+                y = Y,
+                fill = abs(Z))) +
+  coord_equal() +
+  scale_y_discrete(position = "right") +
+  scale_fill_viridis_c(option = "inferno") +
+  labs(x = NULL,
+       y = NULL,
+       fill = TeX("Z")) +
+  theme(legend.position = "NA")
+
+ggsave(file = "T3D_Association_Stair_Plot_Cumulative.tiff",
+       width = 5,
+       height = 4,
+       dpi = 1200)
+
+Legend.Plot = Stair.Plot +
+  geom_tile(data = Sig.Test %>% mutate(X = as.character(X),
+                                       Y = as.character(Y)) %>%
+              filter(X > Y),
+            aes(x = X,
+                y = Y,
+                fill = abs(Z))) +
+  coord_equal() +
+  scale_y_discrete(position = "right") +
+  scale_fill_viridis_c(option = "inferno") +
+  labs(x = NULL,
+       y = NULL,
+       fill = TeX("Z")) +
+  theme(legend.position = "bottom")
+
+ggsave(g_legend(Legend.Plot),
+       file = "T3D_Association_Stair_Plot_Z_Legend.tiff",
+       width = 5,
+       height = 1,
+       dpi = 1200)
